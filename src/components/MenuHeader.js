92088c285abce217f5999ab6de1d11e1112f7873
frknsimsek <furkan.simsek@wenunu.com>
2024-10-31 15:44:50 +0300

    Böh

--- src/components/MenuHeader.js
+++ src/components/MenuHeader.js
@@ -6,7 +6,7 @@ export default function MenuHeader() {
       style={{ backgroundImage: "url('/menu_bg.jpg')", width: '100%', height: '300px' }}
     >
       <div className="flex justify-center py-6">
-        <Image src="/logo.png" alt="Restaurant Logo" className="h-16 w-auto" />
+        <img src="/logo.png" alt="Restaurant Logo" className="h-16 w-auto" />
       </div>
       <h2 className="text-3xl  text-white">Sosunu Seç </h2>
       <h2 className="text-3xl  mb-4 text-white">Dönerin Gelsin</h2>
